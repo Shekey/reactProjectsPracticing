f9291027c32cb67821963b374f90cc01aa1c67c0
Shekey <ajdin_sah@hotmail.com>
2019-09-20 10:08:43 +0200

    added images and other stuffs

---
+++ nba-app/src/hoc/Layout/layout.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './layout.css';
+
+class Layout extends Component {
+  state = {  }
+  render() {
+    return ( <div>
+      header
+      {this.props.children}
+      footer
+    </div> );
+  }
+}
+ 
+export default Layout;
